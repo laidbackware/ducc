a7c7065990019206facf66522ec0e80e627c74f6
Matt Proud <proudmatt@gmail.com>
2020-03-02 15:19:57 +0000

    update credhub image to include itself as a cn

--- 2-prepare-credhub-image.sh
+++ 2-prepare-credhub-image.sh
@@ -3,11 +3,16 @@
 
 set -euxo pipefail
 
-pushd /tmp
+trap 'rm -rf "$TMPDIR"' EXIT
+TMPDIR=$(mktemp -d) || exit 1
+echo "Temp dir is ${TMPDIR}"
+
+pushd /${TMPDIR}
 git clone https://github.com/cloudfoundry-incubator/credhub.git
 pushd credhub
 git checkout 2.5.x
 git checkout 4617496 Dockerfile
+sed -i 's/CN=localhost/CN=credhub/g' ./scripts/setup_dev_mtls.sh*
 docker build . -t credhub/credhub:local-2.5 --rm
 popd
 rm -rf credhub
